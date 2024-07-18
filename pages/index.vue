05e7bd112b83d30307559b3ae1fc86036e3d2724
NoxusJr <ctt.georgejr@gmail.com>
2024-07-18 01:12:32 -0300

    Fix path in BestProject component

--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,8 @@
     
     <main>
         <PortfolioSkills />
-        <PortfolioBestProject :idSection="'desktop-project'" :title="'ToolKit-CLI'" :urlImage="'/_nuxt/assets/images/laptop.png'" :description="'O ToolKit-dev CLI é essencial para desenvolvedores. Gerencie tarefas com uma lista de afazeres, traduza textos instantaneamente e utilize um chat com I.A. para suporte inteligente. Aumente sua produtividade com esta poderosa ferramenta.'" :urlGithub="'https://github.com/georgejrdev/ToolKit-Development'" :urlDeploy="'https://github.com/georgejrdev/ToolKit-Development'" :direction="'row'"/>
-        <PortfolioBestProject :idSection="'mobile-project'" :title="'Post Its'" :urlImage="'/_nuxt/assets/images/cellphone.png'" :description="'O Post Its é o app ideal para capturar ideias e lembretes rapidamente. Com interface intuitiva, permite criar, organizar e personalizar notas facilmente. Use cores, adicione alarmes e sincronize entre dispositivos. Perfeito para tarefas, ideias e lembretes diários.'" :urlGithub="'https://github.com/georgejrdev/Post-Its'" :urlDeploy="'https://play.google.com/store/apps/details?id=com.georgejrdev.postits&pcampaignid=web_share'" :direction="'row-reverse'"/>
+        <PortfolioBestProject :idSection="'desktop-project'" :title="'ToolKit-CLI'" :urlImage="laptopImage" :description="'O ToolKit-dev CLI é essencial para desenvolvedores. Gerencie tarefas com uma lista de afazeres, traduza textos instantaneamente e utilize um chat com I.A. para suporte inteligente. Aumente sua produtividade com esta poderosa ferramenta.'" :urlGithub="'https://github.com/georgejrdev/ToolKit-Development'" :urlDeploy="'https://github.com/georgejrdev/ToolKit-Development'" :direction="'row'"/>
+        <PortfolioBestProject :idSection="'mobile-project'" :title="'Post Its'" :urlImage="cellphoneImage" :description="'O Post Its é o app ideal para capturar ideias e lembretes rapidamente. Com interface intuitiva, permite criar, organizar e personalizar notas facilmente. Use cores, adicione alarmes e sincronize entre dispositivos. Perfeito para tarefas, ideias e lembretes diários.'" :urlGithub="'https://github.com/georgejrdev/Post-Its'" :urlDeploy="'https://play.google.com/store/apps/details?id=com.georgejrdev.postits&pcampaignid=web_share'" :direction="'row-reverse'"/>
         <PortfolioProjects />
         <PortfolioAbout />
         <PortfolioContact />
@@ -15,7 +15,8 @@
 
 
 <script lang="ts" setup>
-
+    import laptopImage from '~/assets/images/laptop.png'
+    import cellphoneImage from '~/assets/images/cellphone.png'
 </script>
 
 
